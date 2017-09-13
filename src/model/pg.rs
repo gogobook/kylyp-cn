675ffaa053c010ee0c9e6b636c34d5962d6d2d87
kriry <krircc@gmail.com>
2017-09-13 11:51:28 +0800

    新开始

--- src/model/pg.rs
+++ src/model/pg.rs
@@ -2,6 +2,8 @@ use postgres::{Connection, TlsMode};
 
 pub fn get_conn() -> Connection {
     
-       Connection::connect("DATABASE_URL", TlsMode::None).unwrap()
+    //    Connection::connect("DATABASE_URL", TlsMode::None).unwrap()
+       Connection::connect("postgres://rforum:rforum@localhost:5432/rforum_cn", TlsMode::None).unwrap()
+
 
 }
